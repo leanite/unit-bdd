41217f6d7007e5e404b2db00ff189974f5dbfaa5
Leandro Leite <leandro@mini-de-leandro.home>
2017-12-22 09:40:58 -0200

    Add Clean Architecture folder structure

--- app/src/main/java/org/bitbucket/lcleite/desafioandroid/ui/MainActivity.java
+++ app/src/main/java/org/bitbucket/lcleite/desafioandroid/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package org.bitbucket.lcleite.desafioandroid;
+package org.bitbucket.lcleite.desafioandroid.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import org.bitbucket.lcleite.desafioandroid.R;
 
 public class MainActivity extends AppCompatActivity {
 
